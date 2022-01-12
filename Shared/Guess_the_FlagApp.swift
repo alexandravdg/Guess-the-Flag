96681b5f6d0da42bdb065045d3a69b3e8fc4a57c
avonderg <77714730+avonderg@users.noreply.github.com>
2022-01-11 22:26:04 -0500

    Initial Commit

---
+++ Shared/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Shared
+//
+//  Created by Alexandra von der Goltz on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
